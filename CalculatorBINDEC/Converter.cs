6b3fe6062a6ca0a36a016dff49ad29cdb44a684b
Ziom1Z1 <kamil.polinski97@gmail.com>
2023-02-01 21:17:47 +0100

    Add project files.

---
+++ CalculatorBINDEC/Converter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorBINDEC
+{
+    internal class Converter
+    {
+        public static string ToDecimal(string input)
+        {
+            return Convert.ToInt64(input, 2).ToString();
+        }
+        public static string ToBinary(string input)
+        {
+            return Convert.ToString(Convert.ToInt64(input), 2);
+        }
+    }
+}
