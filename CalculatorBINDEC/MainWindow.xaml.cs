581838d0c7883ef228c2ebc3d6f3d74bdea519b3
Ziom1Z1 <kamil.polinski97@gmail.com>
2023-03-01 21:36:10 +0100

    Add more numeral systems to GUI

--- CalculatorBINDEC/MainWindow.xaml.cs
+++ CalculatorBINDEC/MainWindow.xaml.cs
@@ -29,29 +29,63 @@ namespace CalculatorBINDEC
         {
             Converter converter = new Converter(textInput.Text);
 
-            if(radioDECBIN.IsChecked== true)
+            var inputSystem = GetInputSystem();
+            var outputSystem = GetOutputSystem();
+
+            if (inputSystem == 10 && converter.ToSystem(outputSystem))
             {
-                if (!converter.ToSystem(2))
-                {
-                    textOutput.Text = "błąd";
-                }
-                else
-                {
-                    textOutput.Text = converter.Value;
-                }
+                textOutput.Text= converter.Value;
+            }
+
+            if (outputSystem == 10 && converter.ToDecimal(inputSystem))
+            {
+                textOutput.Text = converter.Value;
+            }
+
+            if (converter.ToDecimal(inputSystem) && converter.ToSystem(outputSystem))
+            {
+                textOutput.Text = converter.Value;
+            }
+
+
+        }
+        private int GetInputSystem()
+        {
+            if(radioInputBIN.IsChecked == true) 
+            {
+                return 2;
+            }
+            else if (radioInputOCT.IsChecked == true)
+            {
+                return 8;
+            }
+            else if (radioInputDEC.IsChecked == true)
+            {
+                return 10;
             }
             else
             {
-                if (!converter.ToDecimal(2))
-                {
-                    textOutput.Text = "błąd";
-                }
-                else
-                {
-                    textOutput.Text = converter.Value;
-                }
+                return 16;
             }
-
+        }
+        private int GetOutputSystem()
+        {
+            if (radioOutputBIN.IsChecked == true)
+            {
+                return 2;
+            }
+            else if (radioOutputOCT.IsChecked == true)
+            {
+                return 8;
+            }
+            else if (radioOutputDEC.IsChecked == true)
+            {
+                return 10;
+            }
+            else
+            {
+                return 16;
+            }
         }
     }
 }
